83e9d305b11e2c742505c92182275d004b61eecb
carldamey <carldamey@gmail.com>
2023-12-11 22:37:16 -0800

    finishes lessons implementing cats index functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+  {'name': 'Bug', 'breed': 'piebald', 'description': 'pants', 'age': 2},
+  {'name': 'Pooka', 'breed': 'black', 'description': '5 dolar', 'age': 9},
+]
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def cats_index(request):
+  return render(request, 'cats/index.html' , {
+    'cats': cats,
+  })
