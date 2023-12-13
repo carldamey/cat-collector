1329f5643527edbbe4366082acc2f61af054f778
carldamey <carldamey@gmail.com>
2023-12-13 15:22:59 -0800

    implements cat database and cat detail functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-  {'name': 'Bug', 'breed': 'piebald', 'description': 'pants', 'age': 2},
-  {'name': 'Pooka', 'breed': 'black', 'description': '5 dolar', 'age': 9},
-]
+from .models import Cat
 
 # Create your views here.
 def home(request):
@@ -15,6 +9,21 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
+  cats = Cat.objects.all()
   return render(request, 'cats/index.html' , {
     'cats': cats,
   })
+
+def cats_detail(request, cat_id):
+  cat = Cat.objects.get(id=cat_id)
+  return render(request, 'cats/detail.html', {
+    'cat': cat
+  })
+
+
+# cats = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+#   {'name': 'Bug', 'breed': 'piebald', 'description': 'pants', 'age': 2},
+#   {'name': 'Pooka', 'breed': 'black', 'description': '5 dolar', 'age': 9},
+# ]
